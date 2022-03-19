91c07cb11546cecacc5d8d15ef91f122ffc59fd4
luo jun <luojun96@live.cn>
2022-03-19 21:45:46 +0800

    setup project by using spring boot.

---
+++ src/main/java/com/jun/sfi/SfiApplication.java
@@ -0,0 +1,13 @@
+package com.jun.sfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jun/sfi/SfiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.sfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
